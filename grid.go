fb648fff1d7dc25c8d245faab4293bf2764284b5
Tom Gamon <tom@tomgamon.com>
2021-09-13 09:49:16 +1000

    Add new methods

--- grid.go
+++ grid.go
@@ -1,64 +1,95 @@
 package main
 
+import "math/rand"
+
 type Grid struct {
-  rows int
-  cols int
-  matrix CellMatrix
+	rows   int
+	cols   int
+	matrix CellMatrix
 }
 
-type CellMatrix [][]*Cell
+type CellRow []*Cell
+type CellMatrix []CellRow
 
 func NewGrid(rows int, cols int) *Grid {
-  var g Grid
+	var g Grid
 
-  g.rows = rows
-  g.cols = cols
-  g.matrix = prepareGrid(rows, cols)
-  configureCells(g)
+	g.rows = rows
+	g.cols = cols
+	g.matrix = prepareGrid(rows, cols)
+	configureCells(g)
 
-  return &g
+	return &g
 }
 
-func (g Grid) Coord (row int, col int) *Cell{
-  var cell *Cell
+func (g Grid) GetCell(row int, col int) *Cell {
+	var cell *Cell
 
-  if row < 0 || row > g.rows {
-    return cell
-  }
+	if row < 0 || row > g.rows {
+		return cell
+	}
 
-  if col < 0 || col > g.cols {
-    return cell
-  }
+	if col < 0 || col > g.cols {
+		return cell
+	}
 
-  cell = g.matrix[row][col]
+	cell = g.matrix[row][col]
 
-  return cell
+	return cell
+}
+
+func (g Grid) RandomCell() *Cell {
+	randomRow := rand.Intn(g.rows)
+	randomCol := rand.Intn(g.cols)
+	cell := g.GetCell(randomRow, randomCol)
+
+	return cell
+}
+
+func (g Grid) Size() int {
+	return g.rows * g.cols
+}
+
+func (g Grid) EachRow(f func(CellRow)) {
+	for r := 0; r < g.rows-1; r++ {
+		f(g.matrix[r])
+	}
+}
+
+func (g Grid) EachCell(f func(*Cell)) {
+	for r := 0; r < g.rows-1; r++ {
+		for c := 0; c < g.cols-1; c++ {
+			cell := g.matrix[r][c]
+
+			if cell != nil {
+				f(cell)
+			}
+		}
+	}
 }
 
 func prepareGrid(rows int, cols int) CellMatrix {
-  grid := make(CellMatrix, rows)
+	grid := make(CellMatrix, rows)
 
-  for r := range grid {
-    grid[r] = make([]*Cell, cols)
+	for r := range grid {
+		grid[r] = make(CellRow, cols)
 
-    for c := range grid[r] {
-      grid[r][c] = NewCell(r, c)
-    }
-  }
+		for c := range grid[r] {
+			grid[r][c] = NewCell(r, c)
+		}
+	}
 
-  return grid
+	return grid
 }
 
 func configureCells(g Grid) {
-  for r := 0; r < g.rows - 1; r++ {
-    for c := 0; c < g.cols - 1; c++ {
-      cell := g.matrix[r][c] 
+	g.EachCell(func(cell *Cell) {
+		r := cell.row
+		c := cell.col
 
-      cell.north = g.Coord(r - 1,c)
-      cell.south = g.Coord(r + 1,c)
-      cell.west = g.Coord(r,c - 1)
-      cell.east = g.Coord(r,c + 1)
-    }
-  }
+		cell.north = g.GetCell(r-1, c)
+		cell.south = g.GetCell(r+1, c)
+		cell.west = g.GetCell(r, c-1)
+		cell.east = g.GetCell(r, c+1)
+	})
 }
-
--- grid_test.go
+++ grid_test.go
@@ -2,6 +2,7 @@ package main
 
 import(
   "testing"
+  "reflect"
 )
 
 func TestNewGrid(t *testing.T) {
@@ -44,4 +45,30 @@ func TestNewGrid(t *testing.T) {
       t.Errorf("got %v want %v given, %v", got, want, g1)
     }
   })
+}
+
+func TestRandomCell(t *testing.T) {
+  rows := 20
+  cols := 10
+  g1 := NewGrid(rows, cols)
+
+  got := g1.RandomCell()
+  var want *Cell
+
+  if reflect.TypeOf(got).Kind() != reflect.TypeOf(want).Kind() {
+    t.Errorf("got %v want %v given", got, want)
+  }
+}
+
+func TestSize(t *testing.T) {
+  rows := 20
+  cols := 10
+  g1 := NewGrid(rows, cols)
+
+  got := g1.Size()
+  want := rows * cols
+
+  if got != want {
+    t.Errorf("got %v want %v given", got, want)
+  }
 }
