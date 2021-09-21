e4081e091c709c9219c929264ee2abd126ae0d30
Tom Gamon <tom@tomgamon.com>
2021-09-21 12:51:34 +1000

    Add svg rendering

--- main.go
+++ main.go
@@ -1,12 +1,70 @@
 package main
 
 import (
-  "os"
+	"log"
+	"github.com/ajstarks/svgo"
+	"net/http"
 )
 
 func main() {
+	http.Handle("/sidewinder", http.HandlerFunc(sidewinder))
+	http.Handle("/binary", http.HandlerFunc(binaryTree))
+	err := http.ListenAndServe(":2003", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func sidewinder(w http.ResponseWriter, req *http.Request) {
   grid := NewGrid(10,10)
   Sidewinder(grid)
 
-  grid.Output(os.Stdout)
+  renderSvg(grid, w)
+}
+
+func binaryTree(w http.ResponseWriter, req *http.Request) {
+  grid := NewGrid(10,10)
+  BinaryTree(grid)
+
+  renderSvg(grid, w)
+}
+
+func renderSvg(grid *Grid, w http.ResponseWriter) {
+  cellSize := 30
+  offset := 5
+  height := grid.rows * cellSize 
+  width := grid.cols * cellSize 
+  strokeStyle := "stroke:black; stroke-width: 2;"
+
+  w.Header().Set("Content-Type", "image/svg+xml")
+  s := svg.New(w)
+  s.Start(width + 10, height + 10)
+
+  grid.EachCell(func(c *Cell) {
+    x1 := (c.col * cellSize) + offset
+    x2 := ((c.col + 1) * cellSize) + offset
+    y1 := (c.row * cellSize) + offset
+    y2 := ((c.row + 1) * cellSize) + offset
+
+
+    if c.north == nil {
+      s.Line(x1, y1, x2, y1, strokeStyle)
+    }
+
+    if c.west == nil {
+      s.Line(x1, y1, x1, y2, strokeStyle)
+    }
+
+    if !c.Linked(c.east) {
+      s.Line(x2, y1, x2, y2, strokeStyle)
+    }
+
+    if !c.Linked(c.south) {
+      s.Line(x1, y2, x2, y2, strokeStyle)
+    }
+
+  })
+  // grid.Output(w)
+  // s.Line(10, 10, 250, 250, strokeStyle)
+  s.End()
 }
